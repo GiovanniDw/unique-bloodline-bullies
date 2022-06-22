758891a9a286ab9cd8e4047db23c7be18bb6f521
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2022-06-22 15:38:11 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  base: "/unique-bloodline-bullies/",
+  base: '/unique-bloodline-bullies/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
